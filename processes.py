ed010b2a6bc45410f6e780b277926c2a926dd4c3
Leksiin <leksiin73a@mail.ru>
2026-09-15 14:24:48 +0300

    Create processes.py

---
+++ processes.py
@@ -0,0 +1,33 @@
+"""Программа для работы с процессами"""
+import subprocess
+import os
+import multiprocessing as mp
+import time
+
+def main():
+    print("Starting")
+    print(f"PID main: {os.getpid()}")
+    print(f"PID parent main: {os.getppid()}")
+    proc = mp.Process()
+    proc.start()
+    time.sleep(5)
+    welcome()
+
+def welcome():
+    print("Hello")
+    print(f"PID : {os.getpid()}")
+    time.sleep(5)
+
+def work():
+    print("Working")
+    print(f"PID : {os.getpid()}")
+    time.sleep(5)
+    finish()
+
+def finish():
+    print(f"PID : {os.getpid()}")
+    time.sleep(5)
+    print("Finished")
+
+if __name__ == "__main__":
+    main()
